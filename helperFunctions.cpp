4454774d8a2d764f0df35b9adf45633fbc6746f8
Yovana Bozhilov <96024394+yovanabozhilov@users.noreply.github.com>
2025-04-02 15:06:28 +0300

    Update helperFunctions.cpp

--- helperFunctions.cpp
+++ helperFunctions.cpp
@@ -1,4 +1,3 @@
-// helperFunctions.cpp
 #include "helperFunctions.h"
 #include <ctime>
 #include <regex>
@@ -9,7 +8,6 @@
 #include <stdio.h>
 
 std::string maskCardNumber(const std::string& cardNumber) {
-    // Use regex to match the format of the card number and mask the middle digits
     std::regex maskPattern(R"((\d{4})(\d{4})(\d{4})(\d{4}))");
     return std::regex_replace(cardNumber, maskPattern, R"($1-XXXX-XXXX-$4)");
 }
@@ -18,7 +16,6 @@ std::string getUserCountry() {
     char buffer[128];
     std::string result = "";
 
-    // Run curl command to get country from ipinfo.io
     FILE* pipe = popen("curl -s ipinfo.io/country", "r");
     if (!pipe) return "Unknown";
 
@@ -27,7 +24,6 @@ std::string getUserCountry() {
     }
     pclose(pipe);
 
-    // Remove trailing newline character
     result.erase(result.find_last_not_of("\n") + 1);
 
     return result;
@@ -36,9 +32,8 @@ std::string getUserCountry() {
 std::string getCurrentDate() {
     time_t now = time(0);
     tm *ltm = localtime(&now);
-    
-    // Format date and time as YYYY-MM-DD HH:MM:SS
-    char buffer[20];  // Buffer size increased to accommodate time
+
+    char buffer[20];  
     strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", ltm);
     return std::string(buffer);
 }
