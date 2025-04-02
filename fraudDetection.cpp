f6ce0a428de2edb645dc2521d3aa3d75088c69ae
Yovana Bozhilov <96024394+yovanabozhilov@users.noreply.github.com>
2025-04-02 15:05:04 +0300

    Update fraudDetection.cpp

--- fraudDetection.cpp
+++ fraudDetection.cpp
@@ -6,23 +6,20 @@
 #include <unordered_set>
 
 std::string FraudDetection::detectFraud(const TransactionData& transaction) {
-    std::string country = transaction.getCountry();  // Get the country from the transaction
-    double amount = transaction.getAmount();  // Get the amount from the transaction
+    std::string country = transaction.getCountry();  
+    double amount = transaction.getAmount();  
 
     std::string fraudReason = "";
 
-    // Check if the country is suspicious
     if (country != "BG") {
         fraudReason += "Suspicious country";
     } 
 
-    // Check if the amount exceeds the threshold
     if (amount > 18000) {
-        if (!fraudReason.empty()) fraudReason += " & "; // Add separator if there's already a fraud reason
+        if (!fraudReason.empty()) fraudReason += " & "; 
         fraudReason += "Amount exceeds threshold";
     }
 
-    // If there is any fraud reason, return it
     if (!fraudReason.empty()) {
         return "Fraudulent: " + fraudReason;
     }
