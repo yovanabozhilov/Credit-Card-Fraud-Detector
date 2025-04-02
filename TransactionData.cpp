cfa5f35f13bc45f5d6365672b046b0994c7283ff
Yovana Bozhilov <96024394+yovanabozhilov@users.noreply.github.com>
2025-04-02 15:00:23 +0300

    Update TransactionData.cpp

--- TransactionData.cpp
+++ TransactionData.cpp
@@ -1,10 +1,8 @@
 #include "TransactionData.h"
 
-// Constructor definition
 TransactionData::TransactionData(std::string cardNumber, double amount, std::string date, std::string country)
     : cardNumber(cardNumber), amount(amount), date(date), country(country) {}
 
-// Getter function definitions
 std::string TransactionData::getCardNumber() const { return cardNumber; }
 double TransactionData::getAmount() const { return amount; }
 std::string TransactionData::getDate() const { return date; }
