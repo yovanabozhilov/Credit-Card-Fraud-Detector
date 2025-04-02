e0928841ed0f003cf5c716af375727f290401bbf
Yovana Bozhilov <96024394+yovanabozhilov@users.noreply.github.com>
2025-04-02 15:02:17 +0300

    Update TransactionLogger.h

--- TransactionLogger.h
+++ TransactionLogger.h
@@ -1,7 +1,7 @@
 #ifndef TRANSACTION_LOGGER_H
 #define TRANSACTION_LOGGER_H
 
-#include "TransactionData.h"  // Include the TransactionData class
+#include "TransactionData.h"  
 
 void logTransaction(const TransactionData& transaction, const std::string& result);
 
