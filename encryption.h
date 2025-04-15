5ea1815547fee49722a8836620ce4705d7335c26
Yovana Bozhilov <96024394+yovanabozhilov@users.noreply.github.com>
2025-04-15 06:44:06 +0300

    Update encryption.h

--- encryption.h
+++ encryption.h
@@ -7,6 +7,5 @@ extern const unsigned char AES_KEY[16];
 extern const unsigned char AES_IV[16];
 
 std::string encryptData(const std::string& data);
-std::string decryptData(const std::string& encryptedHex);
 
 #endif 
